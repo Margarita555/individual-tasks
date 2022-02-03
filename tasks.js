c2c508ea5b2f4b5b90f12938f7fc65f8a85b045a
Margarita555 <a_rita@ukr.net>
2022-02-03 21:55:15 +0200

    +

--- tasks.js
+++ tasks.js
@@ -388,12 +388,6 @@ console.log(
   ])
 );
 // ============ TASK 16==========================
-
-// ============ TASK 17==========================
-
-// ============ TASK 18==========================
-
-// ============ TASK 19==========================
 function deleteString(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix.length; j++) {
@@ -412,3 +406,29 @@ console.log(
     [4, 4, 0],
   ])
 );
+function deleteColumn(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[i][j] === 0) {
+        for (let i = 0; i < matrix.length; i++) {
+          matrix[i].splice(j, 1);
+        }
+      }
+    }
+  }
+  return matrix;
+}
+console.log(
+  deleteColumn([
+    [1, 1, 1],
+    [2, 0, 2],
+    [3, 3, 3],
+    [4, 4, 0],
+  ])
+);
+
+// ============ TASK 17==========================
+
+// ============ TASK 18==========================
+
+// ============ TASK 19==========================
