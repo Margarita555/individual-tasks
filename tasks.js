75210a516ce5e39853630ba42ea18e8cc2c2e2ba
Kate2120 <gifos.kate@gmail.com>
2022-02-11 11:09:56 +0200

    refactoring

--- tasks.js
+++ tasks.js
@@ -1,3 +1,18 @@
+/*  Как работает принцип работы отлова ошибок с помощью throw, try, catch 
+с учетом вызова ряда функций?*/
+
+// Выполняется код внутри блока try. Если нет ошибок, блок catch игнорируется. 
+// Если есть ошибка, то выполнение блока try прерывается и выполняется catch с переменной error,
+//  которая указываeт, какая ошибка произошла. При использовании try catch наш код не падает, 
+//  обрабатывается ошибка и код выполняется дальше. С помощью оператора throw мы используем 
+//  конструктор new Error и создаём ошибку.
+
+
+/*  Отличие instanceOf от typeof
+
+typeof передает тип данных, а instanceof вернёт true или fslse , проверяет,
+ принадлежит ли object определенному классу с учётом наследования, присутствует ли object в цепочке прототипов*/
+
 "use strict";
 
 String.prototype.mySplit = function (seperator) {
@@ -582,6 +597,7 @@ function transposeMatrix(matrix) {
   }
   return newMatrix;
 }
+ console.log(transposeMatrix([[1,1,1,1,1],[2,2,0,2,2],[3,3,0,3,3]]))
 
 function addMatrixes(matrix1, matrix2) {
   if (!matrix1.length || !matrix2.length) {
