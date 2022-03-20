5cd9fff8d084f17ce8442260add678ddb7b40198
Margarita555 <a_rita@ukr.net>
2022-03-20 09:43:44 +0200

    refactoring

--- block_five.js
+++ block_five.js
@@ -6,18 +6,5 @@ let regexpSite = /^(http|https)\:\/\/(?:[-\w]+\.)?([-\w]+)\.[a-zA-Z]{2,4}?\/?/;
 
 let regexpPassword = /^(?=.*[!@#$%^&*])(?=.*\w)[\w!@#$%^&*]{6,25}$/;
 
-let regexpIpv4 = /^([0-9]{1,3}[\.]){3}[0-9]{1,3}$/;
-
-// let url = /^(http|https)\:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/;
-// console.log(url.test("https://qwertggy.com"));
-console.log(regexpSite.test("https://qwertggy.com"));
-// function validateURL(textval) {
-//   var urlregex = new RegExp(
-//     "^(http|https|ftp)://[a-zA-Z0-9-.]+.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9-._?,'/\\+&amp;%$#=~])*$"
-//   );
-//   return urlregex.test(textval);
-// }
-
-// console.log(validateURL("https://qwertggy.com"));
-
-/^([0-9]{1,3}[\.]){3}[0-9]{1,3}$/.test("999.999.999.999");
+let regexpIpv4 =
+  /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
