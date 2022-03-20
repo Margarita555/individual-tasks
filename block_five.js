0269e3ea306d09bcf802b13df95b9a665573a48e
Margarita555 <a_rita@ukr.net>
2022-03-20 12:02:34 +0200

    Update block_five.js

--- block_five.js
+++ block_five.js
@@ -2,7 +2,7 @@ let regexpTel = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
 
 let regexpEmail = /^[\w-]+@[a-z]+\.[a-z]{2,6}$/;
 
-let regexpSite = /^https?\:\/\/(?:[-\w]+\.)?([-\w]+)\.[a-zA-Z]{2,4}?\/?/;
+let regexpSite = /^https?\:\/\/(?:[-\w]+\.)?[-\w]+\.[a-zA-Z]{2,4}?\/?/;
 
 let regexpPassword = /^(?=.*[!@#$%^&*])(?=.*\w)[\w!@#$%^&*]{6,25}$/;
 
